a18e70a589feb3e6d9a4fac677b417eb6f8ba8c1
edonyM <edonyzpc@gmail.com>
2016-12-18 21:48:19 +0800

    add the function of writing info into mongodb data base

--- strategy_dev/antspi/antspi/settings.py
+++ strategy_dev/antspi/antspi/settings.py
@@ -88,3 +88,10 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+# MongoDB
+ITEM_PIPELINES = {'antspi.pipelines.MongoDBPipeline': 800, }
+MONGODB_SERVER = "localhost"
+MONGODB_PORT = 27017
+MONGODB_DB = "stackoverflow"
+MONGODB_COLLECTION = "questions"
